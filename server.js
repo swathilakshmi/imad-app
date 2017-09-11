3766c61b01779820f517f5083d5e3c792f98a4b1
swathilakshmi <sasikalakasinathan1996@gmail.com>
2017-09-11 16:48:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,14 +77,6 @@ app.get('/:article-Name',function(req,res){
     //2-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     //1-res.send("Article one is requested and will be served here");
 });
-app.get('/article-two',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-    //res.send("Article Two is requested and will be served here");
-});
-app.get('/article-three',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-    //res.send("Article Three is requested and will be served here");
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
