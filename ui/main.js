2f61ed71931d28deccf7e90f4d2f4911a6700518
swathilakshmi <sasikalakasinathan1996@gmail.com>
2017-09-12 17:54:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ element.innerHTML="new Value"
 var img=document.getElementById("madi");
 marginLeft=0;
 function moveRight(){
- marginLeft=marginLeft+10
+ marginLeft=marginLeft+1;
  img.style.marginLeft=marginLeft+'px';
 }
 madi.onclick = function (){
-    var interval=setInterval(moveRight,100)
+    var interval=setInterval(moveRight,50)
   madi.style.marginLeft ='100px';  
 };
