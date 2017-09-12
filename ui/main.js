086e2e5f8ddce7ecc1efb997ecd45cca0e5c1014
swathilakshmi <sasikalakasinathan1996@gmail.com>
2017-09-12 17:47:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="new Value"
-var img=document.getElementById("img");
-img.onclick = function (){
-  img.style.marginLeft ='100px';  
+var img=document.getElementById("madi");
+madi.onclick = function (){
+  madi.style.marginLeft ='100px';  
 };
