3c99727a0c0f3c92f0abe19b1538e8fd544988fb
swathilakshmi <sasikalakasinathan1996@gmail.com>
2017-09-13 08:51:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,11 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+  counter = counter + 1;
+  res.send(counter.toString());
+});
 app.get('/:articleName',function(req,res){
     //articleName will be articleone ,it is express feature
     var articleName=req.params.articleName;
@@ -85,11 +90,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var counter=0;
-app.get('/counter', function (req, res) {
-  counter = counter + 1;
-  res.send(counter.toString());
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
