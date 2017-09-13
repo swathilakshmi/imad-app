d004ce6ca825a46b59fb94366d4a59088d976182
swathilakshmi <sasikalakasinathan1996@gmail.com>
2017-09-13 10:53:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,8 +11,7 @@ madi.onclick = function (){
     var interval=setInterval(moveRight,50);
     madi.style.marginLeft ='100px';  
 };
-var nameInput=document.getElementById('nname');
-var name5=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var request=new XMLHttpRequest();
@@ -20,20 +19,20 @@ submit.onclick=function(){
         if(request.readyState===XMLHttpRequest.DONE){
              if(request.state===200){
                  var names=request.responseText;
-                names=JSON.parse(names);
-                var list='';
-                for(var i3=0;i3<names.length;i3++){
-                    list+='<li>' + names[i3] + '</li>';
-                    }
-                var ul=document.getElementById('namelist');
-                ul.innerHTML=list;
+                 names=JSON.parse(names);
+                 var list='';
+                 for(var i3=0;i3<names.length;i3++){
+                     list+='<li>' + names[i3] + '</li>';
+                     
+                 }
+                 var ul=document.getElementById('namelist');
+                 ul.innerHTML=list;
             
                 }
             }
     };
-   
-   
-
-request.open('GET','http://sasivaya1231996.imad.hasura-app.io/submit-name?name=' + name5,true);
-request.send(null);
+    var nameInput=document.getElementById('nname');
+    var name5=nameInput.value;
+    request.open('GET','http://sasivaya1231996.imad.hasura-app.io/submit-name?name=' + name5,true);
+    request.send(null);
 };
